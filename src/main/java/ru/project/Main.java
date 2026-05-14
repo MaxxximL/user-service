6d0d0ca63dd0014a04a4bc61619baae37bf6264a
MaxxximL <Maxiiiiiiiiim@yandex.ru>
2026-05-14 15:45:12 +0300

    First commit

---
+++ src/main/java/ru/project/Main.java
@@ -0,0 +1,8 @@
+package ru.project;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Запуск UserService");
+        new UserService().start();
+    }
+}
