c0570cd2fd75a09918f142f6845de847ea5cb989
MaxxximL <Maxiiiiiiiiim@yandex.ru>
2026-06-18 01:54:49 +0300

    Commit №7

---
+++ api-gateway/src/main/java/com/userservice/GatewayConfig.java
@@ -0,0 +1,15 @@
+package com.userservice;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("anonymous");
+    }
+}
